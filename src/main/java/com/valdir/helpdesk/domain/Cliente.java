24c5e90f926ff74a1dd7cccb86bda2a943e0681f
Giovandro Santos <vsm.giovandro@gmail.com>
2024-02-24 09:21:23 -0300

    first commit

---
+++ src/main/java/com/valdir/helpdesk/domain/Cliente.java
@@ -0,0 +1,39 @@
+package com.valdir.helpdesk.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.valdir.helpdesk.domain.enums.Perfil;
+
+
+@Entity
+public class Cliente extends Pessoa{
+	private static final long serialVersionUID = 1L;
+
+	@JsonIgnore
+	@OneToMany(mappedBy = "cliente")
+	private List<Chamado> chamados = new ArrayList<>();
+
+	public Cliente() {
+		super();
+		addPerfis(Perfil.CLIENTE);
+	}
+
+	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
+		super(id, nome, cpf, email, senha);
+	}
+
+	public List<Chamado> getChamados() {
+		return chamados;
+	}
+
+	public void setChamados(List<Chamado> chamados) {
+		this.chamados = chamados;
+	}
+	
+	
+}
---
+++ src/main/java/com/valdir/helpdesk/services/TecnicoService.java
@@ -0,0 +1,51 @@
+package com.valdir.helpdesk.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.valdir.helpdesk.services.exceptions.DataIntegrityViolationException;
+import com.valdir.helpdesk.domain.Pessoa;
+import com.valdir.helpdesk.domain.Tecnico;
+import com.valdir.helpdesk.domain.dtos.TecnicoDTO;
+import com.valdir.helpdesk.repositories.PessoaRepository;
+import com.valdir.helpdesk.repositories.TecnicoRepository;
+import com.valdir.helpdesk.services.exceptions.ObjectnotFoundException;
+
+@Service
+public class TecnicoService {
+
+	@Autowired
+	private TecnicoRepository repository;
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
+	public Tecnico findById(Integer id) {
+		Optional<Tecnico> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectnotFoundException("Objecto não encontrado! Id: " + id));
+	}
+
+	public List<Tecnico> findAll() {
+		return repository.findAll();
+	}
+
+	public Tecnico create(TecnicoDTO objDTO) {
+		objDTO.setId(null);
+		validaPorCpfEEmail(objDTO);
+		Tecnico newobj = new Tecnico(objDTO); 
+		return repository.save(newobj);
+	}
+
+	private void validaPorCpfEEmail(TecnicoDTO objDTO) {
+		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
+		if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("CPF já cadastrado no sistema!");
+		}
+		
+		obj = pessoaRepository.findByEmail(objDTO.getEmail());
+		if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("Email já cadastrado no sistema!");
+		}
+	}
+}
