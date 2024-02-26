cf0d2884ae2b5460db37192532ab02c05d59aaa4
Giovandro Santos <vsm.giovandro@gmail.com>
2024-02-26 11:41:35 -0300

    autorizacao com JWT

--- src/main/java/com/valdir/helpdesk/config/SecurityConfig.java
+++ src/main/java/com/valdir/helpdesk/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.valdir.helpdesk.config;
 
 import com.valdir.helpdesk.security.JWTAuthenticationFilter;
+import com.valdir.helpdesk.security.JWTAuthorizationFilter;
 import com.valdir.helpdesk.security.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -42,7 +43,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.cors().and().csrf().disable();
         http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
-//        http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
+        http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
         http.authorizeRequests()
                 .antMatchers(PUBLIC_MATCHERS).permitAll()
                 .anyRequest().authenticated();
